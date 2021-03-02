1d5dc1e85a0cad2279a760f45cbff76bcf2b460e
Edi <selimi.edi.1991@gmail.com>
2021-03-02 12:59:16 +0100

    working on app.js

--- App.js
+++ App.js
@@ -1,7 +1,44 @@
+// Classes
 class Product {
-    constructor(name, price, year){
-        this.name  = name;
-        this.price = price;
-        this.year  = year; 
-    }
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
 }
+
+class UI {
+  addProduct(product) {
+    const productList = document.getElementById("product-list");
+    const element = document.createElement("div");
+    element.innerHTML = `
+          <div class="card text-center mb-4">
+            <div class="card-body">
+               <strong>Product Name</strong>: ${product.name}
+               <strong>Product Price</strong>: ${product.price}
+               <strong>Product year</strong>: ${product.year}
+            </div>
+          </div>
+      `;
+      productList.appendChild(element);
+  }
+
+  deleteProduct() {}
+  showMessage() {}
+}
+
+// DOM Events
+document
+  .getElementById("product-form")
+  .addEventListener("submit", function (e) {
+    let name = document.getElementById("name").value;
+    let price = document.getElementById("price").value;
+    let year = document.getElementById("year").value;
+
+    const product = new Product(name, price, year);
+    
+    const ui = new UI();
+    ui.addProduct(product);
+
+    e.preventDefault();
+  });
